a66ba52711f96931cefc6e10a91cebf3942c4324
Mufidz <mufidz.xtav2.05@gmail.com>
2022-12-21 14:00:14 +0700

    Office Update

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 var_dump($actual_link);
+die;
 if (isset($_SESSION['username'])) {
   if ($_SESSION['role'] == "guru") {
     header("location:guru/kelas.php");
