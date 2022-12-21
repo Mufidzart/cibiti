0659839b936ed29131e3e39dc862bbeb280571e4
Mufidz <mufidz.xtav2.05@gmail.com>
2022-12-21 14:06:57 +0700

    Office Update

--- backend/connection.php
+++ backend/connection.php
@@ -3,10 +3,19 @@ session_start();
 if (!isset($_SESSION['username'])) {
   header("location:../index.php");
 }
-$servername = "localhost";
-$username = "u1671256_cibitiusr";
-$password = "cibitiusr";
-$database = "u1671256_cibiti";
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$uri_segments = explode('/', $actual_link);
+if ($uri_segments[2] == "cibiti.test") {
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "cibiti";
+} else {
+  $servername = "localhost";
+  $username = "u1671256_cibitiusr";
+  $password = "cibitiusr";
+  $database = "u1671256_cibiti";
+}
 
 // membuat koneksi
 $conn = new mysqli($servername, $username, $password, $database);
